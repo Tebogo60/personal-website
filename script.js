1036ea80931ddbecbba0d6ebf7d5422975d042f1
Tebogo60 <Tjjmalebana@gmail.com>
2022-02-22 13:42:19 +0200

    new script file added!

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#open-navbar').click(function(){
+      $('.navigation').toggleClass('show')
+    });
+    
+    $('#open-navbar').click(function(){
+      $('.line1').toggleClass('show')
+      $('.line2').toggleClass('show')
+      $('.line3').toggleClass('show')
+    });
+});
