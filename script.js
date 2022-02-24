3d31f637d6ee970ae72ff2bc5e622ee962c080f0
Tebogo60 <Tjjmalebana@gmail.com>
2022-02-24 16:03:14 +0200

    file updated!

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    $('#open-navbar').click(function(){
-      $('.navigation').toggleClass('show')
-    });
-    
-    $('#open-navbar').click(function(){
-      $('.line1').toggleClass('show')
-      $('.line2').toggleClass('show')
-      $('.line3').toggleClass('show')
-    });
+$(document).ready(function () {
+  $("#open-navbar").click(function () {
+    $(".navigation").toggleClass("show");
+  });
+
+  $(".nav-link").click(function () {
+    $(".navigation").toggleClass("show");
+  });
+
+  $("#open-navbar").click(function () {
+    $(".line1").toggleClass("show");
+    $(".line2").toggleClass("show");
+    $(".line3").toggleClass("show");
+  });
 });
