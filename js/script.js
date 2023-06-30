c2a2fa95c67e4426c02dd665fb3faf34cadd6f8f
Tebogo <tebogo@pop-os.localdomain>
2023-06-30 12:53:22 +0200

    desktop navigation complete

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-$(document).ready(function () {
-  $("#open-navbar").click(function () {
-    $(".navigation").toggleClass("show");
-  });
+const body = document.querySelector("body");
+const navMenu = document.querySelector(".nav-menu");
+const bars = document.querySelector(".bars");
+const light = document.querySelector(".theme-toggle .light");
+const dark = document.querySelector(".theme-toggle .dark");
 
-  $(".nav-link").click(function () {
-    $(".navigation").toggleClass("show");
-  });
-
-  $("#open-navbar").click(function () {
-    $(".line1").toggleClass("show");
-    $(".line2").toggleClass("show");
-    $(".line3").toggleClass("show");
-  });
+bars.addEventListener("click", () => {
+  openMenu();
 });
+
+light.addEventListener("click", () => {
+  changeMode();
+});
+
+dark.addEventListener("click", () => {
+  changeMode();
+});
+
+const openMenu = () => {
+  navMenu.classList.toggle("menu-active");
+  bars.classList.toggle("bars-active");
+};
+
+const changeMode = () => {
+  body.classList.toggle("light-active");
+  light.classList.toggle("theme-active");
+  dark.classList.toggle("theme-active");
+};
