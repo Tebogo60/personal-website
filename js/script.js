873ac428424d88009ea2be24be3ebecfd947d4a0
Tebogo <tebogo@pop-os.localdomain>
2023-07-09 15:13:01 +0200

    added section side indicator functionality

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const moon = document.querySelector(".moon");
 const sun = document.querySelector(".sun");
 const bars = document.querySelector(".bars");
 const menuLink = document.querySelectorAll(".menu-link");
-const section = document.querySelectorAll("section");
 
 let lightActive = false;
 let menuActive = false;
@@ -24,21 +23,6 @@ const changeTheme = () => {
   }
 };
 
-const openMenu = () => {
-  bars.classList.toggle("active_bars");
-  menu.classList.toggle("active_menu");
-};
-
-window.addEventListener("scroll", () => {
-  const scrollValue = window.scrollY;
-
-  if (scrollValue > 69) {
-    navbar.classList.add("blur");
-  } else {
-    navbar.classList.remove("blur");
-  }
-});
-
 themeToggle.addEventListener("click", () => {
   changeTheme();
 });
@@ -51,4 +35,58 @@ menuLink.forEach((e) => {
 
 bars.addEventListener("click", () => {
   openMenu();
+});
+
+const openMenu = () => {
+  bars.classList.toggle("active_bars");
+  menu.classList.toggle("active_menu");
+};
+
+window.addEventListener("scroll", () => {
+  let scrollValue = window.scrollY;
+
+  if (scrollValue > 69) {
+    navbar.classList.add("blur");
+  } else {
+    navbar.classList.remove("blur");
+  }
+});
+
+const sections = document.querySelectorAll("section");
+const indicators = document.querySelectorAll(".indicator span");
+const sectionIdsArr = [];
+const sectionHeightsArr = [];
+
+let sectionHeightValue = 0;
+let currentSection = "home";
+
+sections.forEach((section) => {
+  sectionHeightValue += section.offsetHeight;
+  sectionIdsArr.push(section.id);
+  sectionHeightsArr.push(sectionHeightValue);
+});
+
+window.addEventListener("scroll", () => {
+  let scrollValue = window.scrollY;
+
+  for (let i = 0; i < sectionHeightsArr.length; i++) {
+    if (scrollValue >= sectionHeightsArr[sectionHeightsArr.length - 1]) {
+      currentSection = sectionIdsArr[sectionHeightsArr.length - 1];
+      break;
+    }
+
+    if (scrollValue < sectionHeightsArr[i] - 10) {
+      currentSection = sectionIdsArr[i];
+      break;
+    }
+  }
+
+  indicators.forEach((indicator) => {
+    indicator.classList.remove("active_indicator");
+    if (indicator.className === currentSection) {
+      indicator.classList.add("active_indicator");
+    }
+  });
+
+  console.log(scrollValue);
 });
