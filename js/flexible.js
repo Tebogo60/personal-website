9733e3d451c795c4f4173b1e78ce00e1469748c6
Tebogo <tebogo@pop-os.localdomain>
2023-07-30 20:22:45 +0200

    replace line-awesome with awesome fonts

--- js/flexible.js
+++ js/flexible.js
@@ -1,6 +1,5 @@
 let date = Date().split(" ");
 
 if (parseInt(date[4].slice(0, 2)) > 5 && parseInt(date[4].slice(0, 2)) <= 18) {
-  body.classList.add("light_theme");
-  console.log("body :>> ", body);
+  // body.classList.add("light_theme");
 }
