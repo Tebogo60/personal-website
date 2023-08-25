4026d2165f150ea19c7dea790ceda8fc9d5ef18b
tebogo60 <tjjmalebana@gmail.com>
2023-08-25 23:49:17 +0200

    fixed custom indicator and added background blur light effect!

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-const sectionArr = [
-  "home",
-  "about me",
-  "experience",
-  "project",
-  "get in touch",
-];
-
-for (let i = 0; i < main.children.length; i++) {
-  indicatorSections.innerHTML += `<span class="" id="${i}" style="top: ${
-    (i / 5) * 100
-  }%">${sectionArr[i]}</span>`;
-}
-
-window.addEventListener("scroll", () => {
+const scrollEffects = () => {
   const scrollY = window.scrollY;
 
   if (scrollY > 200) {
@@ -26,41 +12,14 @@ window.addEventListener("scroll", () => {
   indicator.style.top = `${
     (scrollY / (body.offsetHeight + footer.offsetHeight)) * 100
   }%`;
+};
 
-  let currentSectionIndex = Math.floor(
-    (scrollY / (body.offsetHeight + footer.offsetHeight)) * 5
-  );
-
-  let lastSectionId = "";
-  let currentSectionId = sectionArr[currentSectionIndex];
-
-  const addPop = () => {
-    indicatorSections.children[
-      sectionArr.indexOf(lastSectionId) + 1
-    ].classList.add("pop_indicator");
-  };
-
-  if (lastSectionId !== currentSectionId) {
-    lastSectionId = currentSectionId;
-
-    addPop();
-  }
+window.addEventListener("scroll", () => {
+  scrollEffects();
 });
 
 const onloadReturn = () => {
-  const scrollY = window.scrollY;
-
-  if (scrollY > 200) {
-    returnBtn.classList.remove("opacity-0");
-    returnBtn.classList.add("opacity-100");
-  } else {
-    returnBtn.classList.remove("opacity-100");
-    returnBtn.classList.add("opacity-0");
-  }
-
-  indicator.style.top = `${
-    (scrollY / (body.offsetHeight + footer.offsetHeight)) * 100
-  }%`;
+  scrollEffects();
 };
 
 onloadReturn();
@@ -70,11 +29,41 @@ const AnimateButton = () => {
   buttons.forEach((button) => {
     button.id = i++;
     if (parseInt(button.id) % 2 === 0) {
-      button.style.animation = `Animate_button 1s linear 1.5s infinite`;
+      button.style.animation = `Animate_button 1s linear .5s infinite`;
     } else {
-      button.style.animation = `Animate_button 1s linear 1.0s infinite`;
+      button.style.animation = `Animate_button 1s linear 0s infinite`;
     }
   });
 };
 
 AnimateButton();
+
+for (let j = 0; j < 10; j++) {
+  for (let i = 0; i < 10; i++) {
+    boxes.innerHTML += `
+    <span class="box" 
+    style="
+    width: ${Math.floor(bgBox.offsetWidth / 10)}px; 
+    height: ${Math.floor(bgBox.offsetHeight / 10)}px; 
+    top: ${Math.floor(bgBox.offsetHeight / 10) * j}px; 
+    left:${Math.floor(bgBox.offsetWidth / 10) * i}px; 
+    ">
+    </span>;`;
+  }
+}
+
+const randomBoxIndex = () => {
+  return Math.floor(Math.random() * 400);
+};
+
+const usedRandomIndex = [];
+
+const popBox = () => {
+  let randomIndex = randomBoxIndex();
+  boxes.children[randomIndex].classList.toggle("box_pop");
+  usedRandomIndex.push(randomIndex);
+};
+
+if (boxes.children) {
+  setInterval(popBox, 250);
+}
--- js/static.js
+++ js/static.js
@@ -1,9 +1,16 @@
-const main = document.querySelector("main");
 const body = document.querySelector("body");
-const circles = document.querySelector(".circles");
+
+const main = document.querySelector("main");
+
 const returnBtn = document.querySelector(".return");
 const buttons = document.querySelectorAll(".button");
-const indicator = document.querySelector(".indicator");
+
 const indicatorSections = document.querySelector(".indicator_sections");
+const indicator = document.querySelector(".indicator");
+
 const footer = document.querySelector("footer");
+
 const getInTouch = document.querySelector("#getintouch");
+
+const boxes = document.querySelector(".boxes");
+const bgBox = document.querySelector(".bg_box");
